e3b92b704b4d47152dcefb746ac5ea4ff45e9384
nethun5919 <ursula.netherland@bryanuniversity.edu>
2021-03-26 13:35:03 -0700

    add
    adding second project

---
+++ project2.js
@@ -0,0 +1,22 @@
+
+
+var p2 = document.createElement("li");
+newp2.textContent = 'document'
+var list = document.getElementById("List");
+list.appendChild(p2);
+
+
+var h1 = document.createElement("h1");                        
+var text = document.createTextNode("The making of a website.");    
+h1.append(text);                                           
+document.body.appendChild(h1);
+
+
+var paragraph = document.createElement("P");                        
+var text = document.createTextNode("key languages to know when making a website.");    
+paragraph.append(text);                                           
+document.body.appendChild(paragraph);
+
+var Foot = document.createElement("FOOTER");
+var t = document.getElementById("t");
+t.appendChild(foot);
