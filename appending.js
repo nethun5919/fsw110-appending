6665cbe709d32e6870b6ef71c9ef433c66074546
nethun5919 <ursula.netherland@bryanuniversity.edu>
2021-03-26 13:41:05 -0700

    add
    adding to file

---
+++ appending.js
@@ -0,0 +1,19 @@
+
+
+
+var listItems = document.createElement("li");
+newListItem.textContent = 'patience'
+var list = document.getElementById("List");
+list.appendChild(listItems);
+
+
+var h1 = document.createElement("h1");                        
+var text = document.createTextNode("Welcome to my JS site.");    
+h1.append(text);                                           
+document.body.appendChild(h1);
+
+
+var paragraph = document.createElement("P");                        
+var text = document.createTextNode("All of this was created with Javascript.");    
+paragraph.append(text);                                           
+document.body.appendChild(paragraph);
