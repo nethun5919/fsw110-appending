88dea3e6eb9ee6b362e313535f28d0cdbe6f9e23
nethun5919 <ursula.netherland@bryanuniversity.edu>
2021-04-02 18:48:06 -0700

    adding
    these are my first arrays for week 2

---
+++ arraylist.js
@@ -0,0 +1,14 @@
+
+
+
+for (i = 0; i < 9; i++) {
+    text += "Hello World!" + i + "<br>";
+    
+    var h1 = document.createElement("h1");                       
+      var text = document.createTextNode("Hello World!");
+        h1.append(text);
+        document.body.appendChild(h1);
+}       
+
+
+
