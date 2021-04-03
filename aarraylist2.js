8b7a396738c7942e8c2bd1a1490dfcb8c9b72fe8
nethun5919 <ursula.netherland@bryanuniversity.edu>
2021-04-02 18:19:48 -0700

    adding
    this is the array from week 2

---
+++ aarraylist2.js
@@ -0,0 +1,12 @@
+
+
+
+const names = [“steve”, “larry”, “joe”, “shirley”, “steph”, “nate”, “rick”, “emily”];
+
+for (let i = 0;i<names.length; i++){
+  console.log(names[i]);
+
+  let theH2 = document.createElement("list");
+  theh2 = innerText = names[i];
+      names.appendChild(h2)
+}
